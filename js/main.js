a8b9f994eee4e5854f3cd0145f04fcccc4d0f14d
Thai Nguyen <thai.nguyen@aegona.com>
2023-10-01 14:22:09 +0700

    udpate

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
+// header
 $(document).on("click", ".component-header .menu-button", function () {
   $(".component-header .right").toggleClass("active");
+});
+
+// page home
+var swiper = new Swiper(".page-home .promotion-slider", {
+  slidesPerView: 4,
+  spaceBetween: 47,
+  pagination: {
+    el: ".page-home .promotion-slider .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".page-home .promotion .swiper-button-next",
+    prevEl: ".page-home .promotion .swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+
+    600: {
+      slidesPerView: 2,
+    },
+
+    1025: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+$(document).on("click", ".page-home .promotion .button-next", function () {
+  $(".page-home .promotion .swiper-button-next").click();
+});
+$(document).on("click", ".page-home .promotion .button-prev", function () {
+  $(".page-home .promotion .swiper-button-prev").click();
 });
