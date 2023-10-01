a288f527120894691979ee2a3a6e806da146baeb
Thai Nguyen <thai.nguyen@aegona.com>
2023-10-01 16:32:00 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
 // header
 $(document).on("click", ".component-header .menu-button", function () {
   $(".component-header .right").toggleClass("active");
@@ -30,6 +53,15 @@ var swiper = new Swiper(".page-home .promotion-slider", {
     1200: {
       slidesPerView: 4,
     },
+  },
+});
+
+var swiper2 = new Swiper(".page-home .banner-slider", {
+  slidesPerView: 1,
+  spaceBetween: 47,
+  pagination: {
+    el: ".page-home .banner-slider .swiper-pagination",
+    clickable: true,
   },
 });
 
